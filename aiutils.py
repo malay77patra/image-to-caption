db45d78918e30444ffd3ba4745ecedd3760f0acc
malay77patra <malay77patra@gmail.com>
2023-11-16 11:18:10 +0530

    small fix

--- aiutils.py
+++ aiutils.py
@@ -15,4 +15,4 @@ def get_caption(filename):
     with open(filename, "rb") as f:
         data = f.read()
     response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()[0]["generated_text"]
+    return response.json()[0]["generated_text"].replace("arafed", "")
